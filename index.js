137afe50673e1f2f281e4ec620191651a73ed260
Matthew Johnson <mattj20@gmail.com>
2021-04-14 22:59:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function takeANumber(curLine, name) {
   let newArray = []
   let i = 0
   curLine = curLine.push(name)
-  return `Welcome, ${name}. You are number ${(curLine.length)+1} in line.`
+  return `Welcome, ${name}. You are number ${parseInt(curLine.length)+1} in line.`
   return newArray
 }
 
