4574306f68f8f5bb4ddb9a40059d58e2d05b5a2e
Matthew Johnson <mattj20@gmail.com>
2021-04-14 22:37:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function takeANumber(curLine, name) {
   let newArray = []
   let i = 0
-  for (i=0; i<curLine.length; i++) {
-    newArray.push(`Welcome, ${name}. You are number ${(curLine.length)+1} in line.`)
-  }
+  newArray.push(`Welcome, ${name}. You are number ${(curLine.length)+1} in line.`)
   return newArray
 }
 
