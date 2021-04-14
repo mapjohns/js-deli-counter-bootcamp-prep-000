e47c84e214dc24d529c5690c6e5cddbb9f5a2b2c
Matthew Johnson <mattj20@gmail.com>
2021-04-14 22:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function takeANumber(curLine, name) {
   const newArray = []
   let i = 0
-  for (i=0; i<curLine.length;)
+  for (i=0; i<curLine.length;) {
+    
   return `Welcome, ${name}. You are number ${(curLine.length)+1} in line.`
+}
 }
 
 console.log(takeANumber(7,[`Matthew`]))
