89782c5a612025d29b687750f3b4c44d921c40d0
Matthew Johnson <mattj20@gmail.com>
2021-04-14 22:57:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function takeANumber(curLine, name) {
   let newArray = []
   let i = 0
   newArray = curLine.push(name)
-  return newArray
   return `Welcome, ${name}. You are number ${(curLine.length)+1} in line.`
+  return newArray
 }
 
 console.log(takeANumber([],'Matthew'))
